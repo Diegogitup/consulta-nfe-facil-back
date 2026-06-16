650e7a917e57e5644ad036436ddd3d29697d2243
Diego Silva <diego.dsilva51@gmail.com>
2026-06-16 07:27:40 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/api/nfe", async (req, res) => {
+    try {
+        const { chave } = req.body;
+
+        const resposta = await axios.post(
+      "https://consultadanfe.com/api/v1/consulta",
+      {
+        chave,
+      }
+        );
+        res.json(resposta.data);
+    } catch (error) {
+        console.error(error.message);
+
+        res.status(500).json({
+            erro: "Erro ao consultar NF-e",
+        });
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000");
+});
