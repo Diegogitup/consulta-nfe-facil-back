d73afd18e3253d1c6398ae151e4196e45bd2f3ae
Diego Silva <diego.dsilva51@gmail.com>
2026-06-16 07:51:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -27,6 +27,8 @@ app.post("/api/nfe", async (req, res) => {
     }
 });
 
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
